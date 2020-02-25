386cb42ce3a583fc42c66473a57f19bef8664515
Michal Bruzdowski <bruzdowski@gmail.com>
2020-02-25 22:53:38 +0100

    Tabela z pomocami

--- src/component/order/TableHelpComponent.jsx
+++ src/component/order/TableHelpComponent.jsx
@@ -6,8 +6,21 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableContainer from '@material-ui/core/TableContainer';
+import { Typography } from '@material-ui/core';
 
 
+const standardyZywieniowe=[
+    {wiek:4,energiaOd:75,energiaDo:90,plynyOd:80,plynyDo:100,aaOd:1.5,aaDo:1.75,glukozaOd:10,glukozaDo:12,tluszczOd:2,
+        tluszczDo:2.5,sodOd:1,sodDo:1.5,potasOd:0.2,potasDo:0.2,wapnOd:0.2,wapnDo:0.2, fosforOd:0.2,fosforDo:0.2,magnezOd:0.1,magnezDo:0.1},
+    {wiek:8,energiaOd:60,energiaDo:75,plynyOd:60,plynyDo:80,aaOd:1.5,aaDo:1.75,glukozaOd:7,glukozaDo:10,tluszczOd:2,
+        tluszczDo:2.5,sodOd:1,sodDo:1,potasOd:0.1,potasDo:0.1,wapnOd:0.2,wapnDo:0.2,fosforOd:0.1,fosforDo:0.1,magnezOd:0.1,magnezDo:0.1},
+    {wiek:13,energiaOd:30,energiaDo:60,plynyOd:50,plynyDo:70,aaOd:1,aaDo:1.5,glukozaOd:5,glukozaDo:7,tluszczOd:1.5,
+        tluszczDo:2,sodOd:1,sodDo:1,potasOd:0.1,potasDo:0.1,wapnOd:0.2,wapnDo:0.2,fosforOd:0.1,fosforDo:0.1,magnezOd:0.1,magnezDo:0.1},
+    {wiek:18,energiaOd:25,energiaDo:30,plynyOd:30,plynyDo:40,aaOd:1,aaDo:2,glukozaOd:5,glukozaDo:7,tluszczOd:1,
+        tluszczDo:2,sodOd:1,sodDo:2,potasOd:0.1,potasDo:0.5,wapnOd:0.1,wapnDo:0.1,fosforOd:0.1,fosforDo:0.5,magnezOd:0.1,magnezDo:0.2},
+    {wiek:'FAT',energiaOd:20,energiaDo:25,plynyOd:30,plynyDo:40,aaOd:1,aaDo:2,glukozaOd:5,glukozaDo:7,tluszczOd:1,
+        tluszczDo:2,sodOd:1,sodDo:2,potasOd:0.1,potasDo:0.5,wapnOd:0.1,wapnDo:0.1,fosforOd:0.1,fosforDo:0.5,magnezOd:0.1,magnezDo:0.2}
+]
 
 
 class TableHelpComponent extends Component {
@@ -18,7 +31,9 @@ class TableHelpComponent extends Component {
             this.state = {
                 pacjent: props.pacjent,
                 pomiar: props.pomiar,
-                wiek:((new Date()).getFullYear() - (new Date(props.pacjent.dataUrodzenia)).getFullYear())
+                wiek: ((new Date()).getFullYear() - (new Date(props.pacjent.dataUrodzenia)).getFullYear()),
+                pokazPanel:'block',
+                pokazPrzycisk: 'none'
             }
         }
         
@@ -33,7 +48,7 @@ class TableHelpComponent extends Component {
         //Wzór Harrisa Benedicta    
         // 66,437+(13,752*Waga)+(5,003*Wzrost)-(6,755*Wiek) Mężczyzna
         // 655,096+(9,563*Waga)+(1,85*Wzrost)-(4,676*Wiek) Kobieta
-    whb(){      
+    getWhb(){      
         let wynik =0
         if(this.state.pacjent.plec === 'M'){
             wynik = 66.437+(13.752*this.state.pomiar.waga)+(5.003*this.state.pomiar.wzrost)-(6.755*this.state.wiek)
@@ -43,7 +58,38 @@ class TableHelpComponent extends Component {
         }
         return Math.round(wynik)
     }
+    getBmi(){
+       return this.state.pomiar.waga/((this.state.pomiar.wzrost/100)*(this.state.pomiar.wzrost/100)) 
+    }
 
+    getStandard(key){
+        let tmpWiek
+        this.state.wiek > 18 ? tmpWiek = 18 : tmpWiek = this.state.wiek 
+        if(this.getBmi()>=30){
+            return standardyZywieniowe.find(row=> row.wiek>='FAT')[key] * this.state.pomiar.waga
+        }
+        else{
+            return standardyZywieniowe.find(row=> row.wiek>=tmpWiek)[key] * this.state.pomiar.waga
+        }
+    }
+    getPlynyWgKcal(){
+        return this.state.wiek < 18 ? (this.getWzorLonga() * 1.2) : this.getWzorLonga()
+    }
+    plynyWgWagi(){
+        let plynyWgWagi = 0
+        if(this.state.wiek<18){
+            if(this.state.pomiar.waga<10)
+                plynyWgWagi=this.state.pomiar.waga * 100
+            if(this.state.pomiar.waga>10 && this.state.pomiar.waga<20)
+                plynyWgWagi=1000+(20-this.state.pomiar.waga)*50
+            if(this.state.pomiar.waga>20)
+                plynyWgWagi=1500+ (this.state.pomiar.waga-20)*20
+        }
+        else{
+            plynyWgWagi=this.state.pomiar.waga*30
+        }
+        return plynyWgWagi
+    }
 
     /*          Aktywność:
                     LEZY = 1.0
@@ -53,8 +99,8 @@ class TableHelpComponent extends Component {
                     NORMA = 1.0
                     SREDNIO_CIEZKI = 1.1
                     CIEZKI = 1.2  */
-    wzorLonga(){
-        let wynik= this.whb()
+    getWzorLonga(){
+        let wynik= this.getWhb()
         let aktywnosc = 0
         let stanChorego=0
         let temperatura = this.state.pomiar.temperatura
@@ -97,7 +143,7 @@ class TableHelpComponent extends Component {
         }
         wynik= wynik * aktywnosc * stanChorego * temp
 
-        return wynik
+        return Math.round(wynik)
     }
 
     componentDidpdate(){
@@ -108,28 +154,147 @@ class TableHelpComponent extends Component {
         this._isMounted = false;
       }
 
-     
+     onClick(){
+         if(this.state.pokazPanel==='none'){
+            this.setState({
+                pokazPanel: "block",
+                pokazPrzycisk: "none"})
+        }
+         else{
+            this.setState({
+                pokazPanel: "none",
+                pokazPrzycisk: "block"})
+         }
+        
+     }
 
 
 
     render() {
+        const panel={
+            position: "fixed",
+            right: "0px",
+            paddingTop: "10px",
+            paddingBottom: "10px",
+            top:"100px",
+            width: "250px",
+            display: this.state.pokazPanel,
+            borderRadius: '10px',
+            background: 'lightgrey',
+            flexDirection: "row", 
+            alignItems: "streth", 
+            flexWrap: "wrap",
+        }
+        const przycisk={
+            position: "fixed",
+            writingMode: "vertical-rl",
+            textOrientation: "upright",
+            right: "0px",
+            paddingTop: "10px",
+            paddingBottom: "10px",
+            top:"100px",
+            height:"600px",
+            width: "30px",
+            display: this.state.pokazPrzycisk,
+            borderRadius: '10px',
+            background: 'lightgrey',
+
+        }
         let suma=0
         return (
-            <div style={flexStyle}>
-                Wzór Harrisa Benedicta: {this.whb()}
-                 Wzór Longa:{this.wzorLonga()}
+
+        <div>
+            <div style={przycisk} onClick={() => this.onClick()}>
+                <Typography variant="body1" fullwidth align="center"><b>TABELA ZAPOTRZEBOWAŃ</b></Typography>
+            </div>
+
+            <div style={panel} onClick={() => this.onClick()}>
+
+                        <Typography variant="body1" fullwidth align="center"><b>Zapotrzebowanie energetyczne (kcal/dzień)</b></Typography>
+                        <Table size='small'>
+                            <TableBody>
+                                <TableRow >
+                                        <TableCell>Wzór Harrisa Benedicta</TableCell>
+                                        <TableCell align = 'center'>{this.getWhb()}</TableCell>
+                                </TableRow> 
+                                <TableRow >
+                                        <TableCell>Wzór Longa</TableCell>
+                                        <TableCell align = 'center'>{this.getWzorLonga()}</TableCell>
+                                </TableRow> 
+                                <TableRow >
+                                        <TableCell>Standardy Żywieniowe</TableCell>
+                                        <TableCell align = 'center'>{this.getStandard('energiaOd')} - {this.getStandard('energiaDo')}</TableCell>
+                                </TableRow> 
+                            </TableBody>
+                        </Table>
+                        <Typography variant="body1" fullwidth align="center"><b>Podaż substancji organicznych (g/dzień)</b></Typography>
+                        <Table size='small'>
+                            <TableBody>
+                                <TableRow >
+                                        <TableCell>Aminokwasy</TableCell>
+                                        <TableCell align = 'center'>{this.getStandard('aaOd')} - {this.getStandard('aaDo')}</TableCell>
+                                </TableRow> 
+                                <TableRow >
+                                        <TableCell>Glukoza</TableCell>
+                                        <TableCell align = 'center'>{this.getStandard('glukozaOd')} - {this.getStandard('glukozaDo')}</TableCell>
+                                </TableRow> 
+                                <TableRow >
+                                        <TableCell>Tłuszcze</TableCell>
+                                        <TableCell align = 'center'>{this.getStandard('tluszczOd')} - {this.getStandard('tluszczDo')}</TableCell>
+                                </TableRow> 
+                            </TableBody>
+                        </Table>
+                        <Typography variant="body1" fullwidth align="center"><b>Zapotrzebowanie na płyny (ml/dzień)</b></Typography>
+                        <Table size='small'>
+                            <TableBody>
+                                <TableRow >
+                                        <TableCell>Według wagi</TableCell>
+                                        <TableCell align = 'center'>{this.plynyWgWagi()}</TableCell>
+                                </TableRow> 
+                                <TableRow >
+                                        <TableCell>Według kcal</TableCell>
+                                        <TableCell align = 'center'>{this.getPlynyWgKcal()}</TableCell>
+                                </TableRow> 
+                                <TableRow >
+                                        <TableCell>Standardy</TableCell>
+                                        <TableCell align = 'center'>{this.getStandard('plynyOd')} - {this.getStandard('plynyDo')}</TableCell>
+                                </TableRow> 
+                            </TableBody>
+                        </Table>
+                        <Typography variant="body1" fullwidth align="center"><b>Podaż makroskładników (mmol/dzień)</b></Typography>
+                        <Table size='small'>
+                            <TableBody>
+                                <TableRow >
+                                        <TableCell>Na (sód)</TableCell>
+                                        <TableCell align = 'center'>{this.getStandard('sodOd')} - {this.getStandard('sodDo')}</TableCell>
+                                </TableRow> 
+                                <TableRow >
+                                        <TableCell>K (potas)</TableCell>
+                                        <TableCell align = 'center'>{this.getStandard('potasOd')} - {this.getStandard('potasDo')}</TableCell>
+                                </TableRow> 
+                                <TableRow >
+                                        <TableCell>Ca (wapń)</TableCell>
+                                        <TableCell align = 'center'>{this.getStandard('wapnOd')} - {this.getStandard('wapnDo')}</TableCell>
+                                </TableRow> 
+                                <TableRow >
+                                        <TableCell>P (fosfor)</TableCell>
+                                        <TableCell align = 'center'>{this.getStandard('fosforOd')} - {this.getStandard('fosforDo')}</TableCell>
+                                </TableRow> 
+                                <TableRow >
+                                        <TableCell>Mg (magnez)</TableCell>
+                                        <TableCell align = 'center'>{this.getStandard('magnezOd')} - {this.getStandard('magnezDo')}</TableCell>
+                                </TableRow> 
+                            </TableBody>
+                        </Table>
+                        
             </div>   
+        </div>
 
         )
         }
 }
 
 
-const flexStyle={
-    display: "flex",
-    flexDirection: "row", 
-    alignItems: "streth", 
-    flexWrap: "wrap",
-}
+
 
 export default TableHelpComponent;
